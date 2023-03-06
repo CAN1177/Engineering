9ee89bb39b7cfe13cccc73bfc5b47d1f602eecc5
Silva <39960526+CAN1177@users.noreply.github.com>
2023-03-06 14:42:47 +0800

    feat: cssloader、url-loader

---
+++ webpackd定制前端开发环境/lesson3构建基础配置/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: {
+    main: "./src/index.js", // 指定构建入口文件
+  },
+
+  output: {
+    path: path.join(__dirname, "dist/[hash]/"), // 使用 hash
+    filename: "[name].js", // 使用 entry 名称
+    clean: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, // 匹配文件路径的正则表达式，通常我们都是匹配文件类型后缀
+        include: [
+          path.resolve(__dirname, "src"), // 指定哪些路径下的文件需要经过 loader 处理
+        ],
+        use: {
+          loader: "babel-loader", // 指定使用的 loader
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css/,
+        include: [path.resolve(__dirname, "src")],
+        use: [ MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+        ]
+      },
+      {
+        test: /\.(png | jpg | gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {}
+          }
+        ]
+      }
+    ],
+  },
+
+  plugins: [
+    new CopyPlugin([{ from: "src/public", to: "public" }]),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"), // 开发服务器启动路径
+  },
+};
