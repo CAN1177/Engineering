8b3e8ea1eb03e22d2833a4eb1be925aa12206b3e
Silva <39960526+CAN1177@users.noreply.github.com>
2023-04-23 10:03:12 +0800

    feat: JS、其他资源打包和热更新、跨域处理

--- webpack5/src/dist/index.dev.js
+++ webpack5/src/dist/index.dev.js
@@ -1,9 +1,11 @@
-import './index.less';
+"use strict";
+
+require("./index.less");
 
 // eslint-disable-next-line no-console
 console.log('%c Line:2 🌮', 'color:#465975', 'Webpack 5');
 
-const myTest = () => {
+var myTest = function myTest() {
   // eslint-disable-next-line no-console
   console.log('%c Line:19 🥥', 'color:#f5ce50', '99999');
 };
--- webpack5/webpack.config.js
+++ webpack5/webpack.config.js
@@ -1,44 +1,92 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true, // 清理dist目录
-	},
-	module: {
-		rules: [
-			{
-				test: /\.less$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader', 'postcss-loader'],
-			},
-		],
-	},
-	devServer: {
-		hot: true,
-	},
-	plugins: [
-		new HTMLWebpackPlugin({
-			template: './src/index.html',
-			filename: 'index.html',
-			title: '首页',
-		}),
-		new HTMLWebpackPlugin({
-			template: './src/index.html',
-			filename: 'mine.html',
-			title: '我的',
-			minify: {
-				removeComments: true, // 移除注释
-				collapseWhitespace: true, // 移除空格
-			},
-		}),
-		new MiniCssExtractPlugin({
-			filename:'./css/index.css'
-		}),
-		new OptimizeCssAssetsWebpackPlugin()
-	],
-}
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // 清理dist目录
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        // Question: 为什么要加上m?
+        // Answer: 因为有些第三方包的js文件是用es6写的，所以要加上m
+        test: /\.m?js$/,
+        exclude: /node_modules/, // 排除node_modules目录
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  // Question: 为什么要加上useBuiltIns: "usage"?
+                  // Answer: 因为要按需加载polyfill
+                  useBuiltIns: 'usage',
+                  corejs: { version: 3 },
+                  targets: 'defaults',
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    liveReload: true, // 自动刷新
+    compress: true, // 开启gzip压缩
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://api.xdclass.net',
+        pathRewrite: { '^/api': '' }, // 重写路径
+        secure: false, // 如果是https接口，需要配置这个参数
+      },
+      changeOrigin: true, // 支持跨域  覆盖源主机名
+    },
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      title: '首页',
+    }),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: 'mine.html',
+      title: '我的',
+      minify: {
+        removeComments: true, // 移除注释
+        collapseWhitespace: true, // 移除空格
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: './css/index.css',
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new ESLintPlugin({
+      exclude: 'dist', // 排除dist目录
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+      fix: true,
+    }),
+  ],
+};
